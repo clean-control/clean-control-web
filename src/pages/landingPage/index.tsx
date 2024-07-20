762b88dececc65efb91dec5ee01cfe23ff9e22c1
Gabriel Yukio <126337393+GabrielYukioMC@users.noreply.github.com>
2024-07-20 19:23:49 -0300

    dev - atualizacao

---
+++ src/pages/landingPage/index.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import React from 'react';
+
+import NavBar from "../../components/navBar"
+import './style.css';
+
+
+function LandingPage() {
+  return (
+    <>
+      <NavBar />
+      <h1>Landing Page</h1>
+    </>
+  )
+}
+
+export default LandingPage
