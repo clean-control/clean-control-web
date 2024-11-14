a8d72e4da9987165e33adafcb8d7c7b5e4d5eb29
Gabriel Yukio <gabriel.coca@sptech.school>
2024-11-13 22:16:59 -0300

    dev - ajuste

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/landingPage';
-
-
 export default function  App() {
   return (
     <BrowserRouter>
--- src/pages/landingPage/index.jsx
+++ src/pages/landingPage/index.jsx
@@ -1,23 +1,11 @@
 // import { useState } from 'react'
 import React from "react";
 
-import NavBar from "./navBar";
-import SectionStart from "./sectionStart";
-import "./style.css";
-import Section from "./section";
 
 export default function LandingPage() {
   return (
     <>
-      <NavBar />
-      <SectionStart />
-      <Section nome="dmdmd"/>
-      <Section nome="dmdmd"/>
-      <Section nome="dmdmd"/>
-      <Section nome="dmdmd"/>
-      <Section nome="dmdmd"/>
-
-
+     eu sou a landing page
      
     
     </>
