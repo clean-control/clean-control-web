2861da4e1f860e1572a9d1775ca5f5ea2303be11
Gabriel Yukio <gabriel.coca@sptech.school>
2024-12-07 23:15:14 -0300

    dev - itens do ecomm no modelo inicial

--- src/components/ecomm-item/ecommItem.jsx
+++ src/components/ecomm-item/ecommItem.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import './style.css';
+import "./style.css";
 
 export default function EcommItem({ data }) {
-    const { id, title, price, image } = data;
-    return (
-        <div className="ecomm-item" id={id}>
+  const { id, title, image } = data;
+  return (
+    <div className="ecomm-item" id={id}>
+      <div className="box-img-ecomm">
         <img src={image} alt={title} />
+      </div>
+      <div className="box-item-ecomm">
         <h3>{title}</h3>
-        <p>{price}</p>
-        <button type="button">Comprar</button>
+
+<div className="box-btn">
+<button className="btn" type="button">Visitar   <ion-icon name="arrow-forward-outline"></ion-icon></button>
+</div>       
+        <div className="filtro-ecomm-item">
+          {/* icones de vegano, carne, festa, empresa */}
+          <ion-icon name="fast-food-outline"></ion-icon>
+          <ion-icon name="leaf-sharp"></ion-icon>
+          <ion-icon name="balloon-sharp"></ion-icon>
+          <ion-icon name="business-sharp"></ion-icon>
         </div>
-    );
-    }
+      </div>
+    </div>
+  );
+}
